f263f48d638125581bc97345337452b099b722d6
onewoorks <irwanbibrahim@onewoorks.com>
2020-03-07 09:20:09 +0800

    adjust title

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,14 +2,20 @@ import React from 'react'
 import { Row, Col, Card, Skeleton } from 'antd'
 
 import GroupColumn from '../../charts/GroupColumn'
+import Line from '../../charts/Line'
 
 const Dashboard = () => {
     return(
         <>
         <Row gutter={12} className="row-mb">
-            <Col span={24}>
-                <Card className='card-graph' style={{padding:1}}>
+            <Col span={18}>
+                <Card className='card-graph' title="Transaksi Aliran Wang" >
                     <GroupColumn />
+                </Card>
+            </Col>
+            <Col span={6}>
+                <Card className="card-graph" title="Peratus kenaikan">
+                    <Line />
                 </Card>
             </Col>
         </Row>
